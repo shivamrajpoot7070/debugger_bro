fca10a13916a609234467f193646ff08ef0ab821
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-09-25 16:55:55 +0530

    Time: 3 ms (13.39%), Space: 23.3 MB (5.06%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    long long minisum(int i,int j,vector<vector<int>>&arr,vector<vector<int>>&dp){
+    int find(int i,int j,vector<vector<int>>&arr,vector<vector<int>>&dp){
 
-        if(i>=arr.size() || j>=arr[i].size()){
-            return 0;
-        }
-
-        if(i==arr.size()){
+        if(i==arr.size()-1){
             return arr[i][j];
         }
 
-        if(dp[i][j]!=-1){
+        // if(i>=arr.size() || j>=arr[i].size()){
+        //     return INT_MAX;
+        // }
+
+        if(dp[i][j]!=INT_MIN){
             return dp[i][j];
         }
 
-        long long same=arr[i][j]+minisum(i+1,j,arr,dp);
+        int down=arr[i][j]+find(i+1,j,arr,dp);
+        int rd=arr[i][j]+find(i+1,j+1,arr,dp);
 
-        long long next=arr[i][j]+minisum(i+1,j+1,arr,dp);
-
-        return dp[i][j]=min(same,next);
+        return dp[i][j]=min(down,rd);
     }
     int minimumTotal(vector<vector<int>>& arr) {
-        
+
         int i=0;
         int j=0;
-
-        int n=arr.size();
-        int m=arr[0].size();
-
-        vector<vector<int>>dp(201,vector<int>(201,-1));
-
-        return minisum(i,j,arr,dp);
+        vector<vector<int>>dp(201,vector<int>(201,INT_MIN));
+        return find(0,0,arr,dp);
     }
 };
