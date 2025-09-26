968f322378da24e1fdab5e2cb45d6377b95753a0
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-09-26 16:05:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int search(vector<int>&arr, int tar) {
+        
+        int low=0;
+        int high=arr.size()-1;
+
+        while(low<=high){
+
+            int mid=(low+high)/2;
+
+            if(arr[mid]==tar) return mid;
+
+            else if(arr[low]<arr[mid]){
+
+                if(tar<=arr[mid] && tar>=arr[low]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+
+            else{
+
+                if(tar>=arr[mid] && arr[high]>=tar){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+
+        }
+
+        return-1;
+    
+    }
+};
