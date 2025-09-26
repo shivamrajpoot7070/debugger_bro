25e632fff9a776f3016dc851774e68be552370a0
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-09-26 14:44:30 +0530

    Time: 1713 ms (6.09%), Space: 16.6 MB (42.88%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        int ct = 0;
+
+        int i = 0;
+        while (i < n - 2) {
+            int j = i + 1;
+            while (j < n - 1) {
+                int k = j + 1;
+                while (k < n) {
+                    // Since sorted, only one check is enough
+                    if (arr[i] + arr[j] > arr[k]) {
+                        ct++;
+                    }
+                    k++;
+                }
+                j++;
+            }
+            i++;
+        }
+
+        return ct;
+    }
+};
